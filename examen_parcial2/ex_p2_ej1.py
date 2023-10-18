4f1224240463ffd45a5ce727e03cdd778ec3949d
albertocastrovill <josealberto.castro@udem.edu>
2023-10-18 16:05:04 -0600

    examen parcial 2

---
+++ examen_parcial2/ex_p2_ej1.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import norm
+
+# Leer datos y graficar
+data = pd.read_csv('baseballDataset.csv')
+plt.scatter(data['Angulo'], data['Distancia'], color='blue', label='Datos')
+plt.xlabel('Ángulo de Bateo')
+plt.ylabel('Distancia Horizontal Recorrida')
+plt.title('Distancia vs. Ángulo de Bateo')
+
+# Modelo matemático (Polinomial)
+z = np.polyfit(data['Angulo'], data['Distancia'], 2)
+p = np.poly1d(z)
+angulos = np.linspace(0, 80, 5000)
+p_ang = p(angulos)
+plt.plot(angulos, p_ang, 'r-', label='Ajuste polinomial')
+plt.legend()
+
+# Efectividad del modelo
+y_pred = p(data['Angulo'])
+y_actual = data['Distancia']
+R2 = 1 - (np.sum((y_actual - y_pred)**2) / ((len(y_actual) - 1) * np.var(y_actual, ddof=1)))
+print(f'Coeficiente de determinación R^2: {R2:.3f}')
+
+# Sugerencia del Asistente de Bateo
+distancia_objetivo = 150  # Aquí puedes cambiar la distancia que desees
+sugerencias = []
+
+# Como es una ecuación polinomial, lo más probable es que haya dos ángulos con los cuales llegarías a la misma distancia objetivo
+# Por lo tanto será necesario cacular los diferentes ángulos de esta ecuación que dan como resultado la distancia objetivo.
+
+for ang in angulos:
+    dist = p(ang)
+    if abs(dist - distancia_objetivo) < 0.07:  # usando un margen de error de 0.07 para que no haya problemas con la precisión de los números
+        sugerencias.append(ang)
+
+print(f"Sugerencias de ángulo para una distancia de {distancia_objetivo}: {sugerencias}")
+
+
+plt.show()
---
+++ examen_parcial2/ex_p2_ej2.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+import numpy as np
+
+# Open the csv file for reading
+asphaltData = pd.read_csv('ASPHALT01_UDEMPLBLD01F28MOD.csv', header=None)
+earthData = pd.read_csv('EARTH01_UDEMSCFLDF20MOD.csv', header=None)
+grassData = pd.read_csv('GRASS01_UDEMSMBRDF22MOD.csv', header=None)
+
+# Histogram for Data 
+plt.figure(figsize=(8,6))
+
+hist1, bins1, _ = plt.hist(asphaltData[1], bins=15, density=True, alpha=0.7, color='gray', label='Asphalt')
+hist2, bins2, _ = plt.hist(earthData[1], bins=15, density=True, alpha=0.7, color='green', label='Earth')
+hist3, bins3, _ = plt.hist(grassData[1], bins=15, density=True, alpha=0.7, color='blue', label='Grass')
+
+# Fit a normal distribution to each dataset
+params1 = norm.fit(asphaltData[1])
+params2 = norm.fit(earthData[1])
+params3 = norm.fit(grassData[1])
+
+x = np.linspace(min(np.min(asphaltData[1]), np.min(earthData[1]), np.min(grassData[1])),
+                max(np.max(asphaltData[1]), np.max(earthData[1]), np.max(grassData[1])))
+
+plt.plot(x, norm.pdf(x,params1[0],params1[1]), 'r-', label='PDF of Asphalt')
+plt.plot(x, norm.pdf(x,params2[0],params2[1]), 'g-', label='PDF of Earth')
+plt.plot(x, norm.pdf(x,params3[0],params3[1]), 'b-', label='PDF of Grass')
+
+plt.xlabel('Intensity')
+plt.ylabel('Probability Density')
+plt.legend()
+plt.show()
+
+# Abrir el archivo csv para leer
+newData = pd.read_csv('dataSetF12.csv')
+
+newData = newData['intensity'].values
+
+"""
+#Método 1: Usando for loops = lento
+
+# Clasificar cada punto en el nuevo conjunto de datos
+asphalt_count = 0
+earth_count = 0
+grass_count = 0
+
+for point in newData:
+    p_asphalt = norm.pdf(point, loc=params1[0], scale=params1[1])
+    p_earth = norm.pdf(point, loc=params2[0], scale=params2[1])
+    p_grass = norm.pdf(point, loc=params3[0], scale=params3[1])
+    
+    if p_asphalt > p_earth and p_asphalt > p_grass:
+        asphalt_count += 1
+    elif p_earth > p_asphalt and p_earth > p_grass:
+        earth_count += 1
+    else:
+        grass_count += 1
+
+total_points = len(newData)
+print(f"Cantidad de Puntos Analizados: {total_points}")
+print(f"Porcentaje de Asfalto: {(asphalt_count / total_points) * 100:.2f}%")
+print(f"Porcentaje de Tierra: {(earth_count / total_points) * 100:.2f}%")
+print(f"Porcentaje de Pasto: {(grass_count / total_points) * 100:.2f}%")
+"""
+
+# Método 2: Usando máscaras = rápido
+
+# Calcula las densidades de probabilidad
+p_asphalt = norm.pdf(newData, loc=params1[0], scale=params1[1])
+p_earth = norm.pdf(newData, loc=params2[0], scale=params2[1])
+p_grass = norm.pdf(newData, loc=params3[0], scale=params3[1])
+
+# Clasificar datos usando las densidades de probabilidad
+asphalt_mask = (p_asphalt > p_earth) & (p_asphalt > p_grass)
+earth_mask = (p_earth > p_asphalt) & (p_earth > p_grass)
+
+# Contar los puntos clasificados
+asphalt_count = np.sum(asphalt_mask)
+earth_count = np.sum(earth_mask)
+grass_count = len(newData) - asphalt_count - earth_count
+
+# Imprimir resultados
+total_points = len(newData)
+print(f"Cantidad de Puntos Analizados: {total_points}")
+print(f"Porcentaje de Asfalto: {(asphalt_count / total_points) * 100:.2f}%")
+print(f"Porcentaje de Tierra: {(earth_count / total_points) * 100:.2f}%")
+print(f"Porcentaje de Pasto: {(grass_count / total_points) * 100:.2f}%")
+
+
+
+
+
+
+
